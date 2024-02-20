9deaf3c179c692303b5789a3800463b1931e855b
dehyabi <dehyasvibe@gmail.com>
2024-02-20 16:12:44 +0700

    setup prisma to get data from db

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,18 @@
 import express from "express";
 import cors from "cors";
+import { PrismaClient } from "@prisma/client";
 
 
 const app = express();
+const prisma = new PrismaClient();
 
 app.use(express.json());
 app.use(cors());
 
 app.get("/api/notes", async (req, res) => {
- res.json({ message: "success!" });
+ const notes = await prisma.note.findMany(); 
+
+res.json(notes);
 });
 
 
