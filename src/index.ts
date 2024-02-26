02ad88670c61f85cda8694b906f5f0b528e774ae
dehyabi <dehyasvibe@gmail.com>
2024-02-26 09:11:30 +0700

    setup prisma to post data to db and add validation

--- src/index.ts
+++ src/index.ts
@@ -15,6 +15,26 @@ app.get("/api/notes", async (req, res) => {
 res.json(notes);
 });
 
+app.post("/api/notes", async (req, res) => {
+ const { title, content } = req.body;
+
+ if (!title || !content) {
+  return res
+   .status(400)
+   .send("title and content fields required"); 
+} 
+try {
+ const note = await prisma.note.create({
+  data: {title, content}
+ });
+res.json(note);
+ } catch (error) {
+   res
+    .status(500)
+    .send("Oops something went wrong");
+ }
+});
+
 
 app.listen(5000, ()=> {
  console.log("server running on localhost:5000")
